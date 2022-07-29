5de2d40cba23870a69ec9536a8d464c7e10ea39d
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-29 14:22:42 +0900

    Add User Entity in ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot(
+    {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }
+  ), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
