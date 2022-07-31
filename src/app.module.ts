4c9fd7d858b4b97bfdf81aacee789f39254d17c7
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-31 19:26:14 +0900

    Add database env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
@@ -10,12 +11,28 @@ import { Report } from './reports/reports.entity';
 const cookieSession = require('cookie-session');
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true, // WIthout migration, add columns and types in dev
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
+    }),
+
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite', // Nests's recommended way of handling env config is complicated
+    //   entities: [User, Report],
+    //   synchronize: true, // WIthout migration, add columns and types in dev
+    // }),
     UsersModule,
     ReportsModule,
   ],
--- test/auth.e2e-sepc.ts
+++ test/auth.e2e-sepc.ts
@@ -29,4 +29,25 @@ describe('Authentication (e2e)', () => {
         expect(email).toEqual(email);
       });
   });
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+    // manually store  cookies
+    const res = request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
+
+//dev mode ->  for just used as we want
+// test mode ->  wiped
+// auth issue!!
